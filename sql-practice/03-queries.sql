b01bd48cb45bb2aaaaf4cbdef3f8accceacf84fe
Muppana Shanmukha <muppanashanmukha63@gmail.com>
2026-09-10 23:08:33 +0530

    Update 03-queries.sql

--- sql-practice/03-queries.sql
+++ sql-practice/03-queries.sql
@@ -1,1 +1,5 @@
 
+Find the average order value for each city 
+select  avg(o.order_amount),c.city from orders o join customer c on o.order_id=c.cid group by c.city;
+list all orders placed on 2023-07-04and 2027-07-06
+select * from orders where order_date in('2023-07-04','2027-07-06');
