c5e48a5ba3c7baebcd447e51e81ed84a70d0fc0a
Muppana Shanmukha <muppanashanmukha63@gmail.com>
2026-09-15 20:46:40 +0530

    Update 07-queries.sql

--- sql-practice/07-queries.sql
+++ sql-practice/07-queries.sql
@@ -1,1 +1,25 @@
 
+# 📊 LeetCode SQL Solutions
+
+A structured repository containing my solutions to various **LeetCode SQL problems**. This project serves to track my practice, showcase my query optimization skills, and build a strong foundation in data manipulation.
+
+## 🚀 Progress Tracker
+
+| Problem # | Challenge Title | Difficulty | Status | Solution Link |
+| :--- | :--- | :---: | :---: | :---: |
+| 602 | Friend Requests II: Who Has the Most Friends | 🟡 Medium | ⏳ Pending | 
+| 607 | Sales Person | 🟢 Easy | ✅ Solved
+| 608 | Tree Node | 🟡 Medium | SLOVED
+| 610 | Triangle Judgement | 🟢 Easy | ✅ Solved | 
+| 619 | Biggest Single Number | 🟢 Easy | ✅ Solved |
+| 620 | Not Boring Movies | 🟢 Easy | ⏳ Pending | 
+| 626 | Exchange Seats | 🟡 Medium | ⏳ SOLVED
+| 627 | Swap Sex of Employees | 🟢 Easy | ✅ Solved | 
+| 1045 | Customers Who Bought All Products | 🟡 Medium | ⏳ Pending |
+| 1050 | Actors and Directors Who Cooperated At Least Three Times | 🟢 Easy | ⏳ Pending | 
+| 1068 | Product Sales Analysis I | 🟢 Easy | ✅ Solved | 
+| 1070 | Product Sales Analysis III | 🟡 Medium | ⏳ Pending | 
+| 1075 | Project Employees I | 🟢 Easy | ✅ Solved | 
+
+
+
